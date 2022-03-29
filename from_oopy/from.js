935bee32ace83f85f09ec24fca15aa616e89a066
WallistoJH <54902532+WallistoJH@users.noreply.github.com>
2022-03-29 14:54:04 +0900

    Update from.js

--- from_oopy/from.js
+++ from_oopy/from.js
@@ -1,5 +1,5 @@
 jQuery(function($){
-    $(".notion-app-inner > .notion-frame > .notion-scroller > div:first-of-type > img.page_cover").after(''+
+    $(".notion-app-inner > .notion-frame > .notion-scroller > div:first-of-type > img").after(''+
         '<article class="from-header-title">' +
         '   <h6><strong>from.designer</strong></h6>' +
         '   <p>design, from designer!<br/>디자인 그리고 디자이너 경험 이야기</p>' +
