d0cf37c3078948d3ecd04086c145a70bb34f867f
WallistoJH <54902532+WallistoJH@users.noreply.github.com>
2022-03-29 13:54:27 +0900

    Delete from.js

--- from.js
+++
@@ -1,7 +0,0 @@
-jQuery(function($){
-    $(".notion-app-inner > .notion-frame > .notion-scroller > div:first-of-type > img.page_cover").after(''+
-        '<div class="from-header-title">' +
-        '   <h6><strong>from.designer</strong></h6>' +
-        '   <p>design, from designer!<br/>디자인 그리고 디자이너 경험 이야기</p>' +
-        '</div>' );
-});
